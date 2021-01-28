d99db10cbe30f3feca8f77eeaa46b365f945722a
Eng NIYITEGEKA Janvier <65764019+Eng-Janvier@users.noreply.github.com>
2021-01-28 12:24:58 +0200

    Add files via upload

---
+++ Controling home appliances using Blink.ino
@@ -0,0 +1,30 @@
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+char auth[] = "6WNduBFNvwgloM0lZuxkLklbcSElYuPc";
+char ssid[] = "nijas2012";
+char pass[] = "urwombonye";
+int led=D2;
+BLYNK_WRITE(V1)
+{
+int pinValue = param.asInt(); 
+Serial.print("DATA==");
+Serial.println(pinValue);
+if(pinValue==HIGH){
+Serial.println("LED ON");
+digitalWrite(led,HIGH);
+}else{
+ digitalWrite(led,LOW); 
+}
+}
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+  pinMode(led,OUTPUT);
+  Blynk.begin(auth,ssid,pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
